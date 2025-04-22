59056abcd7ec2f27e22691186962e50ff629b0ab
Juan Munoz <juanmunoz890@gmail.com>
2025-04-22 15:14:45 -0300

    add missing relay key

--- crates/rust-eigenda-v2-client/src/tests.rs
+++ crates/rust-eigenda-v2-client/src/tests.rs
@@ -61,7 +61,7 @@ pub fn get_srs_test_config() -> SRSConfig {
 pub fn get_relay_client_test_config() -> crate::relay_client::RelayClientConfig {
     crate::relay_client::RelayClientConfig {
         max_grpc_message_size: 9999999,
-        relay_clients_keys: vec![1, 2],
+        relay_clients_keys: vec![0, 1, 2],
         relay_registry_address: HOLESKY_RELAY_REGISTRY_ADDRESS,
         eth_rpc_url: get_test_holesky_rpc_url(),
     }
