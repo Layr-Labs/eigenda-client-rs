dac5cb1924b91a5a3e40b52fb66d15b765dc38a5
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-06-06 18:04:20 -0300

    Implement alloy signer

--- crates/rust-eigenda-signers/src/signers/ethers.rs
+++ crates/rust-eigenda-signers/src/signers/ethers.rs
@@ -1,8 +1,5 @@
+use alloy::{consensus::SignableTransaction, network::TxSigner, primitives::{Address, FixedBytes, U256}, signers::Signature};
 use ethereum_types::H256;
-use ethers::types::{
-    transaction::{eip2718::TypedTransaction, eip712::Eip712},
-    Signature,
-};
 use thiserror::Error;
 
 use crate::{Message, RecoverableSignature, Sign};
@@ -31,13 +28,13 @@ impl<S> Signer<S> {
 
     async fn sign_digest_with_eip155(
         &self,
-        digest: H256,
+        digest: FixedBytes<32>,
         chain_id: u64,
-    ) -> Result<ethers::types::Signature, Error>
+    ) -> Result<Signature, Error>
     where
         S: Sign,
     {
-        let msg = Message::new(digest.to_fixed_bytes());
+        let msg = Message::new(*digest);
 
         let sig: RecoverableSignature = self
             .inner_signer
@@ -45,95 +42,46 @@ impl<S> Signer<S> {
             .await
             .map_err(|e| Error::Signer(Box::new(e)))?;
 
-        let mut ethers_sig = ethers::types::Signature {
-            r: sig.r.into(),
-            s: sig.s.into(),
-            v: sig.v.to_byte().into(),
-        };
+        let sig_bytes = sig.to_bytes();
 
-        apply_eip155(&mut ethers_sig, chain_id);
+        let alloy_sig = Signature::from_raw(&sig_bytes).unwrap();
 
-        Ok(ethers_sig)
+        //apply_eip155(&mut ethers_sig, chain_id);
+
+        Ok(alloy_sig)
     }
 }
 
 /// Modify the v value of a signature to conform to eip155
-fn apply_eip155(sig: &mut Signature, chain_id: u64) {
+/*fn apply_eip155(sig: &mut Signature, chain_id: u64) {
     let v = (chain_id * 2 + 35) + sig.v;
     sig.v = v;
-}
+}*/
 
 #[async_trait::async_trait]
-impl<T> ethers::signers::Signer for Signer<T>
+impl<T> TxSigner<Signature> for Signer<T>
 where
     T: Sign,
 {
-    type Error = Error;
-
-    /// Signs the hash of the provided message after prefixing it
-    async fn sign_message<S: Send + Sync + AsRef<[u8]>>(
-        &self,
-        message: S,
-    ) -> Result<Signature, Self::Error> {
-        let message = message.as_ref();
-        let message_hash = ethers::utils::hash_message(message);
-
-        self.sign_digest_with_eip155(message_hash, self.chain_id)
-            .await
-    }
 
     /// Signs the transaction
-    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, Self::Error> {
-        let mut tx_with_chain = tx.clone();
-        let chain_id = tx_with_chain
+    async fn sign_transaction(&self, tx: &mut dyn SignableTransaction<Signature>,) -> alloy::signers::Result<Signature> {
+        let chain_id = tx
             .chain_id()
-            .map(|id| id.as_u64())
             .unwrap_or(self.chain_id);
-        tx_with_chain.set_chain_id(chain_id);
+        
+        tx.set_chain_id(chain_id);
 
-        let sighash = tx_with_chain.sighash();
-        self.sign_digest_with_eip155(sighash, chain_id).await
-    }
-
-    /// Encodes and signs the typed data according EIP-712.
-    /// Payload must implement Eip712 trait.
-    async fn sign_typed_data<P: Eip712 + Send + Sync>(
-        &self,
-        payload: &P,
-    ) -> Result<Signature, Self::Error> {
-        let digest = payload
-            .encode_eip712()
-            .map_err(|e| Error::Eip712Encoding(e.to_string()))?;
-
-        let msg = Message::new(digest);
-
-        let sig = self
-            .inner_signer
-            .sign_digest(&msg)
-            .await
-            .map_err(|e| Error::Signer(Box::new(e)))?;
-
-        Ok(ethers::types::Signature {
-            r: sig.r.into(),
-            s: sig.s.into(),
-            v: sig.v.to_byte().into(),
+        let sighash = tx.signature_hash();
+        self.sign_digest_with_eip155(sighash, chain_id).await.map_err(|e| {
+            alloy::signers::Error::Other(Box::new(e))
         })
     }
 
     /// Returns the signer's Ethereum Address
-    fn address(&self) -> ethereum_types::Address {
-        self.inner_signer.public_key().address()
-    }
-
-    /// Returns the signer's chain id
-    fn chain_id(&self) -> u64 {
-        self.chain_id
-    }
-
-    /// Sets the signer's chain id
-    fn with_chain_id<C: Into<u64>>(mut self, chain_id: C) -> Self {
-        self.chain_id = chain_id.into();
-        self
+    fn address(&self) -> Address {
+        let address = self.inner_signer.public_key().address();
+        Address::from_slice(&address.0)
     }
 }
 
@@ -141,12 +89,15 @@ where
 mod tests {
     use std::str::FromStr;
 
-    use ethers::signers::Signer as EthersSignerTrait; /* Alias to avoid naming conflict */
+    use alloy::network::EthereumWallet;
+    use alloy::providers::{Provider, ProviderBuilder};
+    use alloy::rpc::types::TransactionRequest;
+    use alloy::transports::http::reqwest::Url;
+    use alloy::{consensus::TxEip1559, primitives::ruint::aliases::U256};
+    use alloy::primitives::Address;
+    use ethereum_types::H160;
     use ethers::{
         core::rand::thread_rng,
-        middleware::SignerMiddleware,
-        providers::{Http, Middleware, Provider},
-        types::{Address, TransactionRequest, U256},
         utils::{parse_ether, Anvil},
     };
     use tokio;
@@ -155,7 +106,7 @@ mod tests {
     use crate::{signers::private_key::Signer as PrivateKeySigner, SecretKey};
 
     #[tokio::test]
-    async fn test_chain_id_and_address() {
+    async fn test_address() {
         // Given
         let pk_signer = PrivateKeySigner::random(&mut thread_rng());
         let addr = pk_signer.public_key().address();
@@ -165,38 +116,8 @@ mod tests {
         let signer = Signer::new(pk_signer, chain_id);
 
         // Then
-        assert_eq!(signer.chain_id(), chain_id);
-        assert_eq!(signer.address(), addr);
-    }
-
-    #[tokio::test]
-    async fn test_with_chain_id() {
-        // Given
-        let pk_signer = PrivateKeySigner::random(&mut thread_rng());
-        let signer = Signer::new(pk_signer, 1);
-        let new_chain_id = 5u64;
-
-        // When
-        let updated_signer = signer.with_chain_id(new_chain_id);
-
-        // Then
-        assert_eq!(updated_signer.chain_id(), new_chain_id);
-    }
-
-    #[tokio::test]
-    async fn test_sign_message() {
-        // Given
-        let pk_signer = PrivateKeySigner::random(&mut thread_rng());
-        let chain_id = 1u64;
-        let signer = Signer::new(pk_signer, chain_id);
-        let message = "test message";
-
-        // When
-        let signature = signer.sign_message(message).await.unwrap();
-
-        // Then
-        let message_hash = ethers::utils::hash_message(message);
-        signature.verify(message_hash, signer.address()).unwrap();
+        let signer_address = H160::from_slice(signer.address().as_slice());
+        assert_eq!(signer_address, addr);
     }
 
     #[tokio::test]
@@ -206,25 +127,25 @@ mod tests {
         let chain_id = 1u64;
         let signer = Signer::new(pk_signer.clone(), chain_id);
         let to_address = Address::from_str("0x0000000000000000000000000000000000000001").unwrap();
-        let tx = TypedTransaction::Eip1559(ethers::types::Eip1559TransactionRequest {
-            to: Some(to_address.into()),
-            from: Some(signer.address()),
-            data: None,
-            value: Some(parse_ether(1).unwrap()),
-            chain_id: Some(chain_id.into()),
-            max_priority_fee_per_gas: Some(U256::from(1_000_000_000u128)), // 1 Gwei
-            max_fee_per_gas: Some(U256::from(20_000_000_000u128)),         // 20 Gwei
-            gas: Some(U256::from(21_000u64)),
-            nonce: Some(U256::zero()),
+        let mut tx = TxEip1559 {
+            to: alloy::primitives::TxKind::Call(to_address),
+            value: U256::from_str("1000000000000000000").unwrap(), // 1 Ether
+            chain_id,
+            max_priority_fee_per_gas: 1_000_000_000u128, // 1 Gwei
+            max_fee_per_gas: 20_000_000_000u128,         // 20 Gwei
+            gas_limit: 21_000u64,
+            nonce: 0,
             access_list: Default::default(),
-        });
+            input: Default::default(),
+        };
 
         // When
-        let signature = signer.sign_transaction(&tx).await.unwrap();
+        let signature = signer.sign_transaction(&mut tx).await.unwrap();
 
         // Then
-        let sighash = tx.sighash();
-        signature.verify(sighash, signer.address()).unwrap();
+        let sighash = tx.signature_hash();
+        let recovered_addr = signature.recover_address_from_msg(sighash).unwrap();
+        assert_eq!(recovered_addr, signer.address(), "Recovered address should match signer's address");
     }
 
     #[tokio::test]
@@ -233,36 +154,45 @@ mod tests {
         // given
         let anvil = Anvil::new().spawn();
         let endpoint = anvil.endpoint();
-        let provider = Provider::<Http>::try_from(endpoint)
-            .unwrap()
-            .interval(std::time::Duration::from_millis(10u64));
+
+        let rpc_url = Url::from_str(&endpoint).unwrap();
 
         let private_key_hex = SecretKey::new(anvil.keys()[0].to_bytes().into()).unwrap();
         let pk_signer = PrivateKeySigner::new(private_key_hex);
 
         let our_signer = Signer::new(pk_signer, anvil.chain_id());
 
-        let client = SignerMiddleware::new(provider, our_signer.clone());
+        let wallet = EthereumWallet::from(our_signer.clone());
+
+        let provider = ProviderBuilder::new()
+            .wallet(wallet.clone())
+            .on_http(rpc_url.clone());
 
         let from_addr = our_signer.address();
-        let to_addr = anvil.addresses()[1];
+        let to_addr = Address::from_slice(anvil.addresses()[1].as_bytes());
         let value = parse_ether(0.1).unwrap();
 
-        let tx = TransactionRequest::new()
-            .to(to_addr)
-            .from(from_addr)
-            .value(value)
-            .chain_id(anvil.chain_id());
+        let tx = TransactionRequest::from_transaction(TxEip1559 {
+            to: alloy::primitives::TxKind::Call(to_addr),
+            value: U256::from_str("1000000000000000000").unwrap(), // 1 Ether
+            chain_id: anvil.chain_id(),
+            max_priority_fee_per_gas: 1_000_000_000u128, // 1 Gwei
+            max_fee_per_gas: 20_000_000_000u128,         // 20 Gwei
+            gas_limit: 21_000u64,
+            nonce: 0,
+            access_list: Default::default(),
+            input: Default::default(),
+        });
 
         // When
-        let pending_tx = client.send_transaction(tx, None).await;
+        let pending_tx = provider.send_transaction(tx).await.unwrap();
 
         // Then
-        assert!(pending_tx.is_ok());
-        let receipt = pending_tx.unwrap().await.unwrap();
-        assert!(receipt.is_some(), "Transaction should be mined");
+        let receipt = pending_tx.get_receipt().await;
+        //assert!(pending_tx.);
+        assert!(receipt.is_ok(), "Transaction should be mined");
         let receipt = receipt.unwrap();
-        assert_eq!(receipt.status, Some(1u64.into()), "Transaction failed");
+        assert_eq!(receipt.status(), true, "Transaction failed");
         assert_eq!(receipt.from, from_addr);
         assert_eq!(receipt.to, Some(to_addr));
     }
